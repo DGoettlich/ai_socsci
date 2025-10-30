c5892c06d6aedc43159134d1aea80a662ae91547
Urs Florian Maier <ursflorian01@gmail.com>
2025-10-30 11:42:07 +0100

    added text png's for ocr analysis.

--- ocr/pdf2png.py
+++ ocr/pdf2png.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 import fitz  # PyMuPDF
+from tqdm import tqdm
 
-
-pdf_path = Path("data/ocr/magazine.pdf")
+pdf_path = Path("data/ocr/newrecords18.pdf")
+outdir = Path("data/ocr/pages")
+outdir.mkdir(parents=True, exist_ok=True)
 
 doc = fitz.open(pdf_path)
 
-for i, page in enumerate(doc):
+for i, page in tqdm(enumerate(doc), desc="converting", total=len(doc)):
     pix = page.get_pixmap(dpi=300)
-    pix.save(f"data/ocr/magazine_page_{i + 1:03d}.png")
+    pix.save(outdir / f"page_{i + 1:03d}.png")
 
 doc.close()
